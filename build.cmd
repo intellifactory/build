c3df5cc96af4855936cc994917b82bcc94df50da
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2013-07-12 16:39:47 -0400

    Fix build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,13 @@
-tools\NuGet\NuGet.exe install NuGet.Core -version 2.6.0 -o packages
-fsi --exec build.fsx %*
-
+@ECHO OFF
+IF NOT "%NuGetHome%"=="" GOTO :nuget
+SET NuGetHome=tools\NuGet
+:nuget
+"%NuGetHome%\NuGet.exe" install NuGet.Core -version 2.6.0 -o packages
+IF NOT "%FSharpHome%"=="" GOTO :fs
+SET PF=%ProgramFiles(x86)%
+IF NOT "%PF%"=="" GOTO w64
+SET PF=%ProgramFiles%
+:w64
+SET FSharpHome=%PF%\Microsoft SDKs\F#\3.0\Framework\v4.0
+:fs
+"%FSharpHome%\fsi.exe" --exec build.fsx %*
